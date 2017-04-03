e1783c11a8f3a4c4002b70e3a052e3a571a66a26
Xiao Ling <lingxiao@seas.upenn.edu>
2017-04-03 09:38:00 -0400

    rm

--- experiments/run_ppr.py
+++ experiments/run_ppr.py
@@ -5,7 +5,9 @@
 ############################################################
 
 import os
+import json
 import networkx as nx
+from networkx.readwrite import json_graph
 
 from app.config import PATH
 from scripts import *
@@ -19,8 +21,12 @@ bansal = read_gold(PATH['assets']['bansal'])
 
 '''
 	get graph
+	note this is a multi-digraph
+
+	needs to become digraph by converting edges to weighted edges
 '''
-graph = digraph_from_json(PATH['assets']['graph'])
+gr_path = PATH['assets']['graph']
+# digraph = digraph_from_json(PATH['assets']['graph'])
 
 ############################################################
 '''
@@ -30,11 +36,58 @@ words = list(set(join(join(ws) for _,ws in ccb.iteritems())
       + join(join(ws) for _,ws in bansal.iteritems())))
 
 
+############################################################
 '''
-	run PPR on subset of words
+	first thing to do is construct digraph from multi-digraph 
 '''
-ws = words[0:5]
+with open(gr_path, 'r') as f:
+	raw_graph  = json.load(f)
 
-G=nx.Graph()
-G.add_edges_from([(1,2),(1,3)])
-G.add_node("spam")     
+'''
+	now construct edge from counts
+
+	first construct small graph and make sure pagerank
+	runs on it
+'''
+G = nx.DiGraph()
+
+
+
+'''
+	@Use  : Given raw ppdb graph, and two vertices
+	        output edges weighted by counting the 
+	        number of vertices
+	@Given: raw ppdb graph         :: Dict
+			vertiex in the graph u :: String
+			vertiex in the graph v :: String
+	@output: tuple of weight from:
+				u to v, v to u
+'''
+def edge_weight_by_edge_count(raw_graph, u, v):
+	pass
+
+
+# G.add_node('limited' )
+# G.add_node('suicidal')
+# G.add_edge('limited','suicidal', weight=0.9)
+# G.add_edge('suicidal','limited', weight=0.1)
+# pr = nx.pagerank(G, alpha=0.9)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
