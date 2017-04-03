5d67080680921f9d7c0da469b254d59e8cb96a18
Xiao Ling <lingxiao@seas.upenn.edu>
2017-04-02 23:15:50 -0400

    add

---
+++ app/config.py
@@ -0,0 +1,91 @@
+############################################################
+# Module  : All System Paths
+# Date    : March 23rd, 2017
+# Author  : Xiao Ling
+############################################################
+
+import os
+import shutil
+from scripts import Writer
+
+############################################################
+'''
+	System Root
+'''
+root      = os.getcwd()
+input_dir = os.path.join(root, 'input')
+
+# local
+if root[0:6] == '/Users':
+	data_root = '/Users/lingxiao/Documents/research/data/good-great'
+
+# nlp grid
+elif root[0:5] == '/mnt/':
+	data_root = '/nlp/users/xiao/good-great'
+
+else:
+	data_root = ''
+
+'''
+	System Environment
+'''
+PATH = {# directories
+		'directories': {
+	        'log'    : os.path.join(root, 'deploy/logs')
+	       ,'input'  : os.path.join(root, 'inputs'     )
+		},
+		# file paths
+		'assets': {
+			# test sets
+			'ccb'      : os.path.join(root, 'inputs/test/ccb.txt'    )
+		   ,'bansal'   : os.path.join(root, 'inputs/test/bansal.txt' )
+		   ,'anne-25'  : os.path.join(root, 'inputs/test/anne-25.txt' )
+		   ,'anne-125' : os.path.join(root, 'inputs/test/anne-125.txt')
+
+		   # graph
+		   , 'graph'     : os.path.join(root, 'inputs/graph/graph.json')
+		   , 'graph_and' : os.path.join(root, 'inputs/graph/graph_and.json')
+		   , 'graph_or'  : os.path.join(root, 'inputs/graph/graph_or.json')
+
+	}}
+
+
+def setup(PATH,clear_logs):
+
+	os.system('clear')
+
+	log_dir = PATH['directories']['log']
+
+	if clear_logs or os.path.exists(log_dir):
+		shutil.rmtree(log_dir)
+		os.mkdir(log_dir)
+	else:
+		os.mkdir(log_dir)
+
+	writer = Writer(log_dir)
+	writer.tell('Initializing application [ good-great-combo ] ...')
+
+	for _,path in PATH['directories'].iteritems():
+		if not os.path.exists(path):
+			writer.tell('making directory at ' + path)
+			os.mkdir(path)
+		else:
+			writer.tell('directory ' + path + ' already exists')
+
+	for _,path in PATH['assets'].iteritems():
+
+		name = path.split('/')[-1]
+
+		if not os.path.exists(path):
+			writer.tell('Fatal Error: critical asset ' + name + ' not found at ' + path)
+			writer.close()
+			raise NameError('Error: path not found: ' + path)
+		else:
+			writer.tell('Found critical asset ' + name + ' at ' + path)
+
+	writer.tell('complete application setup!')
+	writer.close()
+
+setup(PATH,False)
+
+
---
+++ experiments/run_ppr.py
@@ -0,0 +1,36 @@
+############################################################
+# Module  : Applicaton Main
+# Date    : April 2nd, 2017
+# Author  : Xiao Ling
+############################################################
+
+import os
+import networkx as nx
+
+from app.config import PATH
+from scripts import *
+
+############################################################
+'''
+	read test-set
+'''
+ccb    = read_gold(PATH['assets']['ccb'])
+bansal = read_gold(PATH['assets']['bansal'])
+
+'''
+	get graph
+'''
+graph = digraph_from_json(PATH['assets']['graph'])
+
+############################################################
+'''
+	get set of words from ccb and bansal
+'''
+words = list(set(join(join(ws) for _,ws in ccb.iteritems())
+      + join(join(ws) for _,ws in bansal.iteritems())))
+
+
+'''
+	run PPR on subset of words
+'''
+ws = words[0:5]
